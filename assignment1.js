b43fe715f4427ed91d87ff2c957e7234a954dbb3
christinejavier <cjavier13@ucsbalum.com>
2016-05-04 16:37:55 -0700

    Solves addition algorithm. Sums two numerical inputs.

---
+++ assignment1.js
@@ -0,0 +1,18 @@
+/*
+Assignment: A Plus B
+Problem Description
+
+Problem. Given two digits a and b, find a+b.
+
+Input format. The first line of the input contains two integers a and b (separated by a space).
+
+Constraints. 0≤a,b≤9.
+
+Output format. Output a+b.
+*/
+
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(3, 4));
